671f5311bcbf3f73b684fa1595a6fbf12a7d6a24
maksutovaynur <maksutov@phystech.edu>
2016-07-13 11:47:07 +0200

    AdEx improved

--- brian_test/M.py
+++ brian_test/M.py
@@ -9,59 +9,31 @@ class DummyModel(model_template):
 
 class AdEx(model_template):
     def __init__(self):
-        self._threshold = 'V<-30*mV'
-        self._reset = 'V=4*mV'
-        self.def_inits = {"n": 0.31, "m": 0.05, "h": 0.6}
-        self.monitors_list = {"V": mV}
+        self.def_inits = {"w":0*uA, "V":0*uA}
+        self.monitors_list = {"V": mV, "I": uA, "w":uA}
         self.equations = [
-            "C*dV/dt = I + IL + Ex - w : mV",
+            "dV/dt = (I + IL + Ex - w)/C : mV",
 
-            "tau*dw/dt = a*(V - EL) - w : mA",
+            "dw/dt = (a*(V - EL) - w)/tau : mA",
             "Ex = gL*sF*exp((V - Vt)/sF) : mA",
-            "IL = gL*(EL - V)",
-            "",
+            "IL = gL*(EL - V) : mA",
+            "dI/dt = cI : mA",
             "",
             ""
         ]
         self.params = {
-            "tau": 0.2 * ms,
-            "tau2": 1. * ms,
-            "ENa": -115. * mV,
-            "EK": 12. * mV,
-            "El": -10.613 * mV,
-            "gl": 0.3 * msiemens,
-            "gK": 36.0 * msiemens,
-            "gNa": 120.0 * msiemens,
-            "Vr": 0. * mV,
-            "C": 1. * ufarad,
-            "n": {"a": {
-                "A": 0.01 / mV,
-                "B": -10.0 * mV,
-                "C": 10.0 * mV,
-                "D": 1.0},
-                "b": {
-                    "A": 0.125,
-                    "C": 80.0 * mV}
-            },
-            "m": {"a": {
-                "A": 0.1 / mV,
-                "B": -25.0 * mV,
-                "C": 10.0 * mV,
-                "D": 1.0},
-                "b": {
-                    "A": 4.0,
-                    "C": 18.0 * mV}
-            },
-            "h": {"a": {
-                "A": 0.07,
-                "C": 20.0 * mV},
-                "b": {
-                    "A": 1.,
-                    "B": -30.0 * mV,
-                    "C": 10.0 * mV,
-                    "D": -1.0}
+            "tau": 40 * ms,
+            "EL": 0 * mV,
+            "gL": 0.003 * msiemens,
+            "C": 1 * ufarad,
+            "a": 4 * nS,
+            "b": 0.08 * nA,
+            "sF": 1 * mV,
+            "cI": 0*mA/ms,
+            "Vt": 30*mV,
+            "Vr": 3*mV
             }
-        }
-
+        self._threshold = 'V > %(Vt)f - 0.00001' % self.params
+        self._reset = 'V = %(Vr)f;w+=%(b)f' % self.params
 
 
--- brian_test/single_cell_test.py
+++ brian_test/single_cell_test.py
@@ -5,21 +5,21 @@ defaultclock.dt=0.01*ms
 
 
 timemax = 100
-seq = np.arange(10, timemax-15, 25)
+seq = np.arange(10980, timemax-15, 2)
 spk = zip([0]*len(seq), [i*ms for i in seq])
 
-inits = {'V' : 0*mV, 'I' : 0*uA}
+inits = {'V' : 0*mV, 'I' : 31*uA}
 
 time = timemax*ms
 
 
 myModel = SYM.M.AdEx()
 
-for i in [7]:
+for i in [33]:
     defaultclock.t=0*ms
     dV = i*mV
     SYM.single_cell(myModel, time=time, inits=inits,
-                    spikes = spk, dV= -dV)
+                    spikes = spk, dV= dV)
 
 
 
