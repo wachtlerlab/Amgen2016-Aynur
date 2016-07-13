307d426810fdbf22e9c18c58dd405e0c895de097
maksutovaynur <maksutov@phystech.edu>
2016-07-13 16:29:48 +0200

    Inputs are spikes from other neuron

--- brian_test/single_cell_test.py
+++ brian_test/single_cell_test.py
@@ -4,10 +4,10 @@ import input_signals as I
 
 defaultclock.dt=0.1*ms
 
-timemax = 300
+timemax = 100
 dtstep = 0.5
 
-seq = np.arange(10980, timemax-15, 2)
+seq = np.arange(10, timemax-15, 20)
 spk = zip([0]*len(seq), [i*ms for i in seq])
 
 X = I.gen_time_interval(0, timemax, dtstep)
@@ -17,18 +17,18 @@ Y = I.PeriodicSineFilter(15, 0).on(X, Y)
 print Y
 Y = I.VolFilter(7).on(X, Y)
 
-inits = {'I' : I.TR(Y, mA, dtstep, ms)}
+inits = {'I' : 0*mA} #I.TR(Y, mA, dtstep, ms)}
 
 time = timemax*ms
 
 
 myModel = SYM.M.Izhikevich(inits=inits)
 
-for i in [13]:
+for i in [10, 15, 20, 25]:
     defaultclock.t=0*ms
     dV = i*mV
     SYM.single_cell(myModel, time=time,
-                    spikes = spk, dV= dV)
+                    spikes = spk, dV= dV, monitors={"V":mV}, prefix="(dV = "+str(i)+"mV)")
 
 
 
