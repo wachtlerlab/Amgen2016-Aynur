6bebd92df224cfc70a2920a3411466bed9b3bcbd
maksutovaynur <maksutov@phystech.edu>
2016-07-19 17:33:52 +0200

    advanced plot

--- nix_test/plotFromExperiment.py
+++ nix_test/plotFromExperiment.py
@@ -31,34 +31,48 @@ As = "AfterStimulus"
 
 plt.title("During stimulus membrane potential for exp {0} placed in parallel".format(exp))
 
+aver = 1
+model = 1
 count = len(sec)
-val = count*100+10
+val = (count+aver+model)*100+10
 i = 0
 DATA = []
+xlabel, ylabel = "x", "y"
 
 for sc in sec:
     i+=1
     if not Bs in sc or not As in sc or not Ds in sc: continue
     median = np.median(np.concatenate((sc[Bs].magnitude, sc[As].magnitude)))
+    xlabel = "Time, $"+str(sc[Ds].times.units)+"$"
+    ylabel = "Potential, $"+str(sc[Ds].units)+"$"
     x = sc[Ds].times - sc[Ds].times[0]
     x = x[x<1.0]
     y = (sc[Ds].magnitude-median)[:len(x)]
     DATA.append({"x":x, "y":y, "xunits":sc[Ds].times.units, "yunits":sc[Ds].units,
-                 "xlabel":"Time, $"+str(sc[Ds].times.units)+"$",
-                 "ylabel":"Potential, $"+str(sc[Ds].units)+"$",
+                 "name":str(i),
                  "median":median,
                  "place":val+i
                  })
-i = 0
-for k in DATA:
-    i+=1
-    #plt.subplot(k["place"])
-    plt.plot(k["x"], k["y"], label=str(i))
-    plt.xlabel(k["xlabel"])
-    plt.ylabel(k["ylabel"])
 
-Y = np.array([k["y"] for k in DATA]).mean(axis=0)
-plt.plot(DATA[0]["x"], Y, label = "average")
 
-plt.legend()
+if aver:
+    Y = np.array([k["y"] for k in DATA]).mean(axis=0)
+    DATA.append({"x":DATA[0]["x"], "y":Y, "name":"average",
+                "median":np.mean([k["median"] for k in DATA]),
+                "place":val+count+1})
+
+if model:
+    x = res.times[res.times<1.0]
+    y = res.magnitude[:len(x)]
+    DATA.append({"x":x, "y":y, "name":"fitting",
+                "median":None,
+                "place":val+count+2})
+numsset = range(1, 100)
+plotset = set(map(str, numsset)+["average","fitting"])
+for k in [k for k in DATA if k["name"] in plotset]:
+    plt.subplot(k["place"])
+    plt.plot(k["x"], k["y"], label=k["name"])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
 plt.show()
